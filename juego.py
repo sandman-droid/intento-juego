9d5cf443879a73e78a1fcba6e624e1031ea04bf6
sandman-droid <brayanjose1098@hotmail.com>
2020-12-27 12:09:25 -0600

    Iniciando algunas clases que puedo usar

---
+++ juego.py
@@ -0,0 +1,40 @@
+#Intentar crear el aspecto de un personaje 2D
+import random
+import pygame
+Ancho=800
+Alto=400
+Posicion_personaje=None
+# Funcion
+
+
+#1)Clases
+class Personaje:
+    def __init__(self):
+        pass
+    def __str__(self):
+        pass
+    def alforja(self):
+        pass
+    def Dibujar(self,pos,lienzo):
+        for n in range(5):
+            canvas.draw_polygon([[400,200],[401,200],[400,201],[401,201]],1,"Red","Red")
+
+
+class Cuadro:
+    def __init__(self):
+        pass
+    def __str__(self):
+        pass
+    def cambiar_estado(self):
+        pass
+#2) Funciones de ayuda
+def Encontrar_posicion_del_personaje():
+    pass
+
+#3) Definir controladores de eventos
+def dibujar(canvas):
+    canvas.draw_polygon([[400,200],[401,200],[400,201],[401,201]],1,"Red","Red")
+
+
+
+#4) Crear marco
